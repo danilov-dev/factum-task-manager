a670aabf3425c19d00ba4c7a3eb162e8906a351e
danilov-dev <dan.forth.ant@gmail.com>
2026-07-06 17:44:47 +0300

    Added post create form

---
+++ apps/posts/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+from apps.posts.models import Post
+
+
+class CreatePostForm(forms.ModelForm):
+    """Форма создания поста"""
+
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={
+                'class': 'form-control markdown-textarea',
+                'rows': 200,
+                'cols': 80,
+                'style': 'width: 100%; min-height: 500px;',
+            }),
+        }
